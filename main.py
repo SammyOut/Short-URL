4365fb0334ce86e190cc08fa43ef46c917dd16f4
Nerd-Bear <python@istruly.sexy>
2018-12-13 09:26:52 +0900

    short url using dictionary

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-from flask import Flask
+from flask import Flask, request, redirect, Response
 
 app: Flask = Flask(__name__)
+short_url = {}
+index = 1
+
+
+def base62_encode() -> str:
+    base62 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    global index
+
+    temp = []
+    while index > 0:
+        temp.append(base62[index % 62])
+        index //= 62
+
+    index += 1
+    return ''.join(temp[::-1])
 
 
 @app.route('/', methods=['POST'])
-def make_short_url():
-    pass
+def make_short_url() -> Response:
+    original_url = request.json['url']
+
+    for short, original in short_url.items():
+        if original == original_url:
+            key = short
+            break
+    else:
+        key = base62_encode()
+        short_url[key] = original_url
+
+    return Response('http://localhost/' + key, 201)
 
 
 @app.route('/<key>', methods=['GET'])
-def get_url(key):
-    pass
+def get_url(key) -> Response:
+    if key not in short_url.keys():
+        return Response('', 204)
+
+    return redirect(short_url[key], 302)
 
 
 if __name__ == '__main__':
